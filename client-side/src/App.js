72cfcd1d6a69eaa54175851fc0572850ba4ba989
LaFriska <harold.gao205@gmail.com>
2024-12-04 22:23:59 +1100

    added logo

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,10 +1,12 @@
 import './css/App.css';
 import ControlPanel from "./components/ControlPanel";
+import Logo from "./components/Logo";
 
 
 function App() {
     return (
         <div>
+            <Logo></Logo>
             <ControlPanel></ControlPanel>
         </div>
     );
---
+++ client-side/src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../css/Logo.css'
+const Logo = props => {
+    return(
+        <div className="logoContainer">
+            <h1>ELIXIR</h1>
+            <h3>
+                Pixel Texture Editor
+            </h3>
+        </div>
+    )
+}
+
+export default Logo
