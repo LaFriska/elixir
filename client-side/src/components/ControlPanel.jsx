118b0376b360c90c96433d95224f818f3fa15bb8
LaFriska <harold.gao205@gmail.com>
2024-12-04 23:29:31 +1100

    added some images

--- client-side/src/components/ControlPanel.jsx
+++ client-side/src/components/ControlPanel.jsx
@@ -4,12 +4,25 @@ import '../css/App.css';
 const ControlPanel = props => {
     return (
         <div id="control" className="control">
-            <p>Name</p>
-            <input type="text" id="nameInput" placeholder="Enter a name" style={{width: "100px"}}/>
-            <p>Dimensions</p>
-            <input type="number" id="widthInput" style={{width: "15px", height: "15px", textAlign: "center"}}/>
-            <p style={{paddingLeft: 5, paddingRight: 5}}>×</p>
-            <input type="number" id="heightInput" style={{width: "15px", height: "15px", textAlign: "center"}}/>
+            <div id="control-left" className="control-left">
+                <p>Name</p>
+                <input type="text" id="nameInput" placeholder="Enter a name" style={{width: "100px"}}/>
+                <p>Dimensions</p>
+                <input type="number"
+                       id="widthInput"
+                       defaultValue="8"
+                       style={{width: "15px", height: "15px", textAlign: "center"}}/>
+                <p style={{paddingLeft: 5, paddingRight: 5}}>×</p>
+                <input type="number"
+                       id="heightInput"
+                       defaultValue="8"
+                       style={{width: "15px", height: "15px", textAlign: "center"}}/>
+            </div>
+            <div id="control-right" className="control-right">
+                {/*<img src="./img/bucket.png"/>*/}
+                <button>Export to PNG</button>
+                <button>Export to JSON</button>
+            </div>
         </div>
     );
 };
