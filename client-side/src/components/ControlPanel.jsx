8bb0a43fe15cc12ab452bc86c230f574b7722fa3
LaFriska <harold.gao205@gmail.com>
2024-12-04 15:20:08 +1100

    improved css and added dimensions field

--- client-side/src/components/ControlPanel.jsx
+++ client-side/src/components/ControlPanel.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../css/Controller.css';
+import '../css/App.css';
 const ControlPanel = props => {
     return (
         <div id="control" className="control">
             <p>Name</p>
-            <input type="text" id="nameInput" defaultValue="Untitled"/>
+            <input type="text" id="nameInput" placeholder="Enter a name" style={{width: "100px"}}/>
+            <p>Dimensions</p>
+            <input type="number" id="widthInput" style={{width: "15px", height: "15px", textAlign: "center"}}/>
+            <p style={{paddingLeft: 5, paddingRight: 5}}>×</p>
+            <input type="number" id="heightInput" style={{width: "15px", height: "15px", textAlign: "center"}}/>
         </div>
     );
 };
