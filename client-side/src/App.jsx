e2308558cfc11d876735b7a3cf5a40805cd6c174
LaFriska <harold.gao205@gmail.com>
2024-12-05 11:59:45 +1100

    added width and height states

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,13 +1,20 @@
 import './css/App.css';
 import ControlPanel from "./components/ControlPanel";
 import Logo from "./components/Logo";
-
+import {useEffect, useState} from "react";
 
 function App() {
+
+    //Height and width of the canvas
+    const [width, setWidth] = useState(8)
+    const [height, setHeight] = useState(8)
+
     return (
         <div>
             <Logo></Logo>
-            <ControlPanel></ControlPanel>
+            <ControlPanel setHeight={setHeight}
+                          setWidth={setWidth}>
+            </ControlPanel>
         </div>
     );
 }
--- client-side/src/components/ControlPanel.jsx
+++ client-side/src/components/ControlPanel.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import '../css/Controller.css';
 import '../css/App.css';
+
+/**
+ * Represents a single input field for a dimension variable (either width or height).
+ * @param props contains a single setter that either sets the width constant or height constant.
+ * immediately when the input number is changed.
+ */
+const DimensionInput = props => {
+    return (
+        <input type="number"
+               id="widthInput"
+               defaultValue="8"
+               style={{width: "30px", height: "15px", textAlign: "center"}}
+               onChange={(e) => {
+                   const val = Number.parseInt(e.target.value);
+                   if(!isNaN(val)) props.set(val)
+               }}
+        />
+    )
+}
+
+/**
+ * Represents the control panel toward the top of the webpage. Contains fields providing ways to
+ * set names, dimensions etc for the image.
+ * @param props contains setter methods as a way to retrieve information to the parent.
+ */
 const ControlPanel = props => {
+
     return (
         <div id="control" className="control">
             <div id="control-left" className="control-left">
                 <p>Name</p>
                 <input type="text" id="nameInput" placeholder="Enter a name" style={{width: "100px"}}/>
                 <p>Dimensions</p>
-                <input type="number"
-                       id="widthInput"
-                       defaultValue="8"
-                       style={{width: "15px", height: "15px", textAlign: "center"}}/>
+                <DimensionInput set={props.setWidth}></DimensionInput>
                 <p style={{paddingLeft: 5, paddingRight: 5}}>×</p>
-                <input type="number"
-                       id="heightInput"
-                       defaultValue="8"
-                       style={{width: "15px", height: "15px", textAlign: "center"}}/>
+                <DimensionInput set={props.setHeight}></DimensionInput>
             </div>
             <div id="control-right" className="control-right">
-                {/*<img src="./img/bucket.png"/>*/}
                 <button>Export to PNG</button>
                 <button>Export to JSON</button>
             </div>
