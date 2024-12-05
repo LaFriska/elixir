4568f27abacc076b40c5810f4034764bb20e81b5
LaFriska <harold.gao205@gmail.com>
2024-12-05 22:30:27 +1100

    added canvas component

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
                           setWidth={setWidth}
                           setName={setName}
             />
-
         </div>
     );
 }
---
+++ client-side/src/components/Canvas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Canvas = props => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Canvas
